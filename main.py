a612d4daeed902b13d56b568050141338f121230
Kaleb-Aguirre03 <820930@mystma.org>
2021-10-05 08:55:50 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+#Define the function howLong() using len() in order to find how long a string is (# of characters in the array)
+def howLong(w):
+    print(len(w))
+
+#run the function in order to test it
+howLong("Dragon Ball Z")
